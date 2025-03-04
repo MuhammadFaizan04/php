124c7b92d42371252063f1f4794397d0993ae5aa
faizan <faizanirshad260@gmail.com>
2025-03-03 23:18:54 -0800

    admin panel

--- adminPanel/php/query.php
+++ adminPanel/php/query.php
@@ -1,10 +1,47 @@
 <?php
 include("dbcon.php");
 
+
+$categoryName = $categoryDes = $categoryImageName = "";
+$categoryNameErr = $categoryDesErr = $categoryImageNameErr = "";
+
 if(isset($_POST['addCategory'])){
     $categoryName = $_POST['cName'];
     $categoryDes = $_POST['cDes'];
-    $categoryImage = $_FILES[]
+    $categoryImageName = $_FILES['cImage']['name'];
+    $categoryImageTmpName = $_FILES['cImage']['tmp_name'];
+    $extension = pathinfo($categoryImageName,PATHINFO_EXTENSION);
+    $destination = "images/".$categoryImageName;
+    $categoryImageSize = $_FILES['cImage']['size'];
+    if(empty($categoryName)){
+        $categoryNameErr = "Category Name is Required";
+    }
 
+    if(empty($categoryImageName)){
+        $categoryImageNameErr = "Category Image is Required";
+    }
+
+    else{
+        $format = ["jpg" , "png" , "jpeg" , "webp"];
+        if(!in_array($extension,$format)){
+            $categoryImageNameErr = "invalid Extension";
+        }
+    }
+
+    if(empty($categoryDes)){
+        $categoryDesErr = "Category Description is Required";
+    }
+
+    if(empty($categoryNameErr) && empty($categoryDesErr) && empty($categoryImageNameErr)){
+        if(move_uploaded_file($categoryImageTmpName,$destination)){
+            $query = $pdo->prepare("insert into categories(name , description , image) values (:cName , :cDes , :cImage)");
+            $query->bindParam('cName',$categoryName);
+            $query->bindParam('cDes',$categoryDes);
+            $query->bindParam('cImage',$categoryImageName);
+            $query->execute();
+            echo "<script>alert('category added');location.assign('addCategory.php')</script>";
+        }
+
+    }
 }
 ?>
